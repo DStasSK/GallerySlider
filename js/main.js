c462e9d5264c9e8634d4763b7403199a38b923c6
DStasSK <DStasSK@yadnex.ru>
2022-07-07 12:54:33 +0300

    finished add left position for items

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
-let gallery = document.querySelector('.gallery');
-let galleryShadow = document.querySelector('.galleryShadow');
+const gallery = document.querySelector('.gallery');
+const galleryShadow = document.querySelector('.galleryShadow');
+const testColor = document.querySelector('.test_color');
 let galleryItemPull = 20;
 
 const backgroundColor = [
@@ -11,27 +12,59 @@ const backgroundColor = [
 const backgroundURL = [];
 
 // left position at %
-if(galleryItemPull>4) {let lefPositionPull = [0, 2, 5, 10, 90, 95, 98, 100];}
-if(galleryItemPull==4){let lefPositionPull = [0, 2, 5, 10, 90, 95, 98];}
-if(galleryItemPull==3){let lefPositionPull = [0, 5, 10, 90, 95];}
-if(galleryItemPull<=2){let lefPositionPull = [0, 10, 90];}
-
-// random start
-let checkedIndex = Math.round(Math.random()*galleryItemPull);
-// let checkedIndex = 0;
-// console.log(checkedIndex);
-
-for(let i=0; i < galleryItemPull; i++){
-	let div = document.createElement('div');
-	setLeftPosition(div);
-	div.style.backgroundColor = backgroundColor[i];
-	gallery.appendChild(div);
+let lefPositionPull, a;
+if(galleryItemPull>4) {
+	lefPositionPull = [0, 2, 5, 10, 90, 95, 98, 100];
+	a = 3;
+}
+if(galleryItemPull==4){
+	lefPositionPull = [0, 2, 5, 10, 90, 95, 98];
+	a = 3;
+}
+if(galleryItemPull==3){
+	lefPositionPull = [0, 5, 10, 90, 95];
+	a = 2;
+}
+if(galleryItemPull<=2){
+	lefPositionPull = [0, 10, 90];
+	a = 1;
 }
 
-function setLeftPosition(item){
-	let left = '10%';
-	// if(i<checkedIndex){}
-	item.style.left=left;
+// random start
+let checkedIndex = Math.round(Math.random()*(galleryItemPull - 1));
+// checkedIndex=0;
+
+
+// стартовая генерация галереи
+for(let i = 0; i < galleryItemPull; i++){
+	let div = document.createElement('div');
+	setLeftPosition(div, i);
+	div.style.backgroundColor = backgroundColor[i];
+	gallery.appendChild(div);
+
+	// тестовая полоска цветов
+	let div2 = document.createElement('div');
+	div2.style.backgroundColor = backgroundColor[i];
+	testColor.appendChild(div2);
+}
+
+
+function setLeftPosition(item, i){
+	let j = i - checkedIndex;
+	let left;
+	if(j < 0){
+		left = lefPositionPull[Math.max(j + a, 0)] + '%';
+	}
+	if(j == 0){
+		if(i==0){left = '0'}
+		else {left = lefPositionPull[a] + '%';}
+	}
+	if(j > 0){
+		let c = lefPositionPull.length - 1;
+		left = lefPositionPull[Math.min(a + j, c)] + '%';
+	}
+	if(i==0){left = '0'}
+	item.style.left = left;
 }
 
 function checkItem(){
